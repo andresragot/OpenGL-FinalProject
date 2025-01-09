fe3d5cde074d1b7be983d33566bc6cf177d2e0a1
Andrés Ragot <andresragot99@gmail.com>
2025-01-09 22:22:52 +0100

    scene camera movements

--- code/MySystem.cpp
+++ code/MySystem.cpp
@@ -41,10 +41,11 @@ namespace Ragot
     
     void System::initialize()
     {
-        glViewport   (0, 0, window.get_width(), window.get_height());
         glEnable     (GL_CULL_FACE );
         glEnable     (GL_DEPTH_TEST);
         glClearColor (0.1f, 0.1f, 0.1f, 1.f);
+        
+        scene.resize (window.get_width(), window.get_height());
     
         kernel.add (make_shared <Critical_Task> (buffer_swap   ));
         kernel.add (make_shared <Critical_Task> (scene_render  ));
@@ -88,6 +89,12 @@ namespace Ragot
     
     void System::input()
     {
+        int  mouse_x     = 0;
+        int  mouse_y     = 0;
+        bool button_down = false;
+        
+        vec3 camera_translation (0);
+        
         while (true)
         {
             SDL_Event event;
@@ -101,12 +108,123 @@ namespace Ragot
                 eventQueue.pop();
             }
             
-            string eventString = getEventString(event);
-            cout << "Event: " << eventString << endl;
-            
-            if (event.type == SDL_QUIT)
+            switch (event.type)
             {
-                stop();
+                case SDL_KEYDOWN:
+                {
+                    switch (event.key.keysym.sym)
+                    {
+                        case SDLK_w:
+                            camera_translation.z = -1.f;
+                        break;
+                        case SDLK_s:
+                            camera_translation.z = +1.0f;
+                        break;
+                        case SDLK_a:
+                            camera_translation.x = -1.f;
+                        break;
+                        case SDLK_d:
+                            camera_translation.x = +1.0f;
+                        break;
+                        case SDLK_q:
+                            camera_translation.y = -1.0f;
+                        break;
+                        case SDLK_e:
+                            camera_translation.y = +1.0f;
+                        break;
+                        case SDLK_LSHIFT:
+                        case SDLK_RSHIFT:
+                            auto shift_task = make_shared < Once_Task > ([&] { scene.on_shift_pressed(true); });
+                            kernel.add (shift_task);
+                        break;
+                    }
+                    
+                    auto keyboard = make_shared < Once_Task > ([&] {scene.on_translation (camera_translation);});
+                    kernel.add (keyboard);
+                    
+                    break;
+                }
+                
+                case SDL_KEYUP:
+                {
+                    switch (event.key.keysym.sym)
+                    {
+                        case SDLK_w:
+                        case SDLK_s:
+                            camera_translation.z = 0.0f;
+                        break;
+                        
+                        case SDLK_a:
+                        case SDLK_d:
+                            camera_translation.x = 0.0f;
+                        break;
+                        
+                        case SDLK_q:
+                        case SDLK_e:
+                            camera_translation.y = 0.0f;
+                        break;
+                        
+                        case SDLK_LSHIFT:
+                        case SDLK_RSHIFT:
+                            auto shift_task = make_shared < Once_Task > ([&] { scene.on_shift_pressed(false); });
+                            kernel.add (shift_task);
+                        break;
+                        
+                    }
+                    
+                    auto keyboard = make_shared < Once_Task > ([&] {scene.on_translation (camera_translation);});
+                    kernel.add (keyboard);
+                    
+                    break;
+                }
+                
+            
+                case SDL_MOUSEBUTTONDOWN:
+                {
+                    auto   buttons = SDL_GetMouseState (&mouse_x, &mouse_y);
+                    auto left_down = buttons &  SDL_BUTTON(SDL_BUTTON_LEFT);
+                    
+                    if (left_down && not button_down)
+                    {
+                        auto mouse_task = std::make_shared < Once_Task >([&] { scene.on_click (mouse_x, mouse_y, button_down = true); });
+                        kernel.add (mouse_task);
+                    }
+                    
+                    break;
+                }
+                
+                case SDL_MOUSEBUTTONUP:
+                {
+                    auto   buttons = SDL_GetMouseState(&mouse_x, &mouse_y);
+                    auto left_down = buttons & SDL_BUTTON(SDL_BUTTON_LEFT);
+                    
+                    if (not left_down && button_down)
+                    {
+                        auto mouse_task = std::make_shared < Once_Task > ([&] { scene.on_click (mouse_x, mouse_y, button_down = false);});
+                        kernel.add (mouse_task);
+                    }
+                    
+                    break;
+                }
+                
+                case SDL_MOUSEMOTION:
+                {
+                    SDL_GetMouseState (&mouse_x, &mouse_y);
+                    auto mouse_task = std::make_shared < Once_Task > ([&] {scene.on_drag (mouse_x, mouse_y); });
+                    kernel.add (mouse_task);
+                    
+                    break;
+                }
+                
+                case SDL_QUIT:
+                {
+                    stop();
+                    
+                    break;
+                }
+                
+                default:
+                    break;
             }
         }
     }
@@ -135,6 +253,92 @@ namespace Ragot
         }
     }
     
+    void Scene::resize(int width, int height)
+    {
+        this->width = width;
+        this->height = height;
+    
+        camera.set_ratio (float(width) / height);
+        
+        glViewport   (0, 0, width, height);
+    }
+    
+    void Scene::on_drag(int pointer_x, int pointer_y)
+    {
+        lock_guard <mutex> lock (scene_mutex);
+        if (pointer_pressed)
+        {
+            angle_delta_x = 0.0000025f * float (last_pointer_y - pointer_y) / float (height);
+            angle_delta_y = 0.0000025f * float (last_pointer_x - pointer_x) / float (width );
+        }
+    }
+    
+    void Scene::on_click(int pointer_x, int pointer_y, bool down)
+    {
+        lock_guard <mutex> lock (scene_mutex);
+        
+        if ((pointer_pressed = down) == true)
+        {
+            last_pointer_x = pointer_x;
+            last_pointer_y = pointer_y;
+        }
+        else
+        {
+            angle_delta_x = angle_delta_y = 0.0f;
+        }
+    }
+    
+    void Scene::render()
+    {
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    }
+    
+    void Scene::update()
+    {
+        lock_guard <mutex> lock (scene_mutex);
+        angle_around_x += angle_delta_x;
+        angle_around_y += angle_delta_y;
+        
+        if (angle_around_x > +1.5f)
+        {
+            angle_around_x = +1.5f;
+        }
+        else
+        if (angle_around_x < -1.4f)
+        {
+            angle_around_x = -1.4f;
+        }
+        
+        vec3 camera_forward = glm::normalize (vec3 (camera.get_target()) - vec3 (camera.get_location()));
+        vec3   camera_right = glm::normalize (glm::cross(camera_forward, vec3 (0.f, 1.f, 0.f)));
+        vec3      camera_up = glm::vec3(0.f, 1.f, 0.f);
+        
+        vec3 new_translation = camera_translation.x * camera_right + camera_translation.y * camera_up - camera_translation.z * camera_forward;
+        camera_translation = new_translation * camera_speed;
+        
+        if (turbo) camera_translation *= camera_turbo_speed;
+        
+        camera.move(camera_translation);
+        
+        // cout << "angle_x: " << angle_around_x << "\n" << "angle_y: " << angle_around_y << endl;
+        
+        glm::mat4 camera_rotation(1);
+        
+        camera_rotation = glm::rotate (camera_rotation, angle_around_y, glm::vec3(0.f, 1.f, 0.f));
+        camera_rotation = glm::rotate (camera_rotation, angle_around_x, glm::vec3(1.f, 0.f, 0.f));
+        
+        camera.set_target (0, 0, -1);
+        camera.rotate     (camera_rotation);
+
+    }
+    
+    void Scene::on_translation (vec3 translation)
+    {
+        lock_guard <mutex> lock (scene_mutex);
+    
+        camera_translation = translation;
+    }
+    
     void Scene::add_entities(shared_ptr<Entity> entity, const string & name)
     {
         if (!entities.contains(name))
@@ -142,6 +346,13 @@ namespace Ragot
             entity->set_scene(this);
             entities.emplace (name, entity);
         }
+    }
+    
+    void Scene::on_shift_pressed(bool down)
+    {
+        lock_guard <mutex> lock (scene_mutex);
+        
+        turbo = down;
     }
     
     void Scene::remove_entities(const string &name)
@@ -153,7 +364,4 @@ namespace Ragot
             entities.erase(iterator);
         }
     }
-    
-    
-    
 }
--- code/MySystem.hpp
+++ code/MySystem.hpp
@@ -10,6 +10,7 @@
 #include "Window.hpp"
 #include "Task.hpp"
 #include "MyKernel.hpp"
+#include "Camera.hpp"
 #include <string>
 #include <memory>
 #include <map>
@@ -29,7 +30,42 @@ namespace Ragot
     class Scene
     {
     public:
+        Camera camera;
         
+    private:
+        
+        mutex scene_mutex;
+    
+        int width;
+        int height;
+        
+        float  angle_around_x;
+        float  angle_around_y;
+        float  angle_delta_x;
+        float  angle_delta_y;
+        
+        float camera_speed = 0.0025f;
+
+        bool   pointer_pressed;
+        int    last_pointer_x;
+        int    last_pointer_y;
+        bool   turbo;
+        
+        float camera_turbo_speed = 2.f;
+        
+        glm::vec3 camera_translation;
+        
+    public:
+        void resize (int width, int height);
+        void on_drag (int pointer_x, int pointer_y);
+        void on_click (int pointer_x, int pointer_y, bool down);
+        void on_translation (glm::vec3 translation);
+        void on_shift_pressed(bool down);
+        
+    public:
+        
+        void update();
+        void render();
     
     private:
         map <string, shared_ptr <Entity>> entities;
@@ -46,6 +82,7 @@ namespace Ragot
         Critical_Task scene_render;
         Light_Task    scene_update;
         Light_Task    process_events;
+        
         
         queue<SDL_Event> eventQueue;
         mutex queueMutex;
