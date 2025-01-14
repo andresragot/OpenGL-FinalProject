262ad3113ca991529852326e3dbd33c258e650a3
Andrés Ragot <andresragot99@gmail.com>
2025-01-14 23:34:16 +0100

    copy right

--- code/Mesh.hpp
+++ code/Mesh.hpp
@@ -1,10 +1,31 @@
-//
-//  Mesh.hpp
-//  OpenGL-FinalProject
-//
-//  Created by Andrés Ragot on 4/1/25.
-//
-
+/*
+ *  This file is part of OpenGL-FinalProject
+ *
+ *  Developed by Andrés Ragot - github.com/andresragot
+ *
+ *  MIT License
+ *
+ *  Copyright (c) 2024 Andrés Ragot
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ */
+ 
 #pragma once
 
 #include <glad/glad.h>
--- code/main.cpp
+++ code/main.cpp
@@ -1,75 +1,35 @@
-//
-//  main.cpp
-//  OpenGL-FinalProject
-//
-//  Created by Andrés Ragot on 31/12/24.
-//
-
-/*#include <iostream>
-#include <thread>
-#include "Task.hpp"
-#include "MyKernel.hpp"
-
-using namespace Ragot;
-
-// Colores para debuggear.
-const char*    RED = "\033[1;31m";
-const char* YELLOW = "\033[1;33m";
-const char*  RESET = "\033[0m";
-const char*  GREEN = "\033[1;32m";
-
-int main (int , char * [])
-{
-    /// TODO: Desarrollar el resto de Componentes:
-    ///  * Mesh
-    ///  * Camera
-    ///  * Transform
-    ///
-    /// TODO: Luego hacer todos los controllers.
-    /// 
-    
-    return 0;
-}*/
-
-/*// constructing atomics
-#include <iostream>       // std::cout
-#include <atomic>         // std::atomic, std::atomic_flag, ATOMIC_FLAG_INIT
-#include <thread>         // std::thread, std::this_thread::yield
-#include <vector>         // std::vector
-
-std::atomic<bool> ready (false);
-std::atomic_flag winner = ATOMIC_FLAG_INIT;
-
-void count1m (int id) {
-  while (!ready) { std::this_thread::yield(); }      // wait for the ready signal
-  for (volatile int i=0; i<1000000; ++i) {}          // go!, count to 1 million
-  if (!winner.test_and_set()) { std::cout << "thread #" << id << " won!\n"; }
-};
-
-int main ()
-{
-  std::vector<std::thread> threads;
-  std::cout << "spawning 10 threads that count to 1 million...\n";
-  for (int i=1; i<=10; ++i) threads.push_back(std::thread(count1m,i));
-  ready = true;
-  for (auto& th : threads) th.join();
-
-  return 0;
-}
-*/
-
-#include <iostream>
-#include <SDL.h>
-#include <glad/glad.h>
+/*
+ *  This file is part of OpenGL-FinalProject
+ *
+ *  Developed by Andrés Ragot - github.com/andresragot
+ *
+ *  MIT License
+ *
+ *  Copyright (c) 2024 Andrés Ragot
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ */
+ 
 #include "Entity.hpp"
 #include "MySystem.hpp"
 #include "Component.hpp"
-#include "Window.hpp"
-#include "Camera.hpp"
-#include "Mesh.hpp"
-#include <SOIL2.h>
 #include <memory>
-#include "Color_Buffer.hpp"
 
 using namespace Ragot;
 using namespace std;
@@ -81,10 +41,8 @@ int main(int argc, char* argv[])
     
     auto mesh_entity = make_shared < Entity > ();
 
-    auto mesh_component = std::make_shared < Model_Component > ("Intergalactic_Spaceship-(Wavefront).obj", "fotos/Intergalactic Spaceship_color_4.jpg");
+    auto mesh_component = make_shared < Model_Component > ("Intergalactic_Spaceship-(Wavefront).obj", "fotos/Intergalactic Spaceship_color_4.jpg");
     mesh_entity->add_components(mesh_component, "Mesh");
-    // mesh_entity->transform.set_rotation({0.f, 90.f, 0.f});
-    // mesh_entity->transform.set_scale({0.01f, 0.01f, 0.01f});
     mesh_entity->transform.set_position({0.f, 0.f, -10.f});
     my_system.add_entities(mesh_entity, "Mesh");
     
